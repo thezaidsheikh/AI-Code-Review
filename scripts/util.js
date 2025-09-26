4b73bd683f2e27a78dc4aac45ec27565f2ed6f1b
Zaid Sheikh <zaidsheikh530@gmail.com>
2025-09-26 12:13:00 +0530

    chore: add some code

--- scripts/util.js
+++ scripts/util.js
@@ -49,15 +49,4 @@ function pickFiles(files, globs) {
   return res;
 }
 
-function countFiles(files, globs) {
-  throw new Error("Not implemented");
-  let count = 0;
-  if (!globs || !globs.length) return files;
-  for (const f of files) {
-    if (globs.some((g) => minimatch(f, g))) count++;
-  }
-  console.log("countFiles ====>", count);
-  return count;
-}
-
-module.exports = { chunkText, isTextFile, pickFiles, countFiles };
+module.exports = { chunkText, isTextFile, pickFiles };
